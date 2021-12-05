d1c7a487b529bec1ed137e5970bf08a23df47e3b
Joshua Vega <jvega@jsvcycling.com>
2021-12-05 11:09:21 -0500

    day 2: improve code

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,18 +1,28 @@
 package com.jsvcycling.aoc2021
 
 object Day2 extends Day {
-  override type Input = (String, Int)
+  override type Input = Command
   override type Output = Int
 
-  override def parseInput(input: LazyList[String]): LazyList[Input] = input.map(_.split(' ').take(2)).map {
-    case Array(a, b) => (a, b.toInt)
+  sealed trait Command
+  case class Forward(amt: Int) extends Command
+  case class Up(amt: Int) extends Command
+  case class Down(amt: Int) extends Command
+  case class Unknown() extends Command
+
+  override def parseInput(input: LazyList[String]): LazyList[Input] = input.map(_.split(' ')).map {
+    case Array(a, b, _*) if a == "forward" => Forward(b.toInt)
+    case Array(a, b, _*) if a == "up" => Up(b.toInt)
+    case Array(a, b, _*) if a == "down" => Down(b.toInt)
+    case _ => Unknown()
   }
 
   override def part1(input: LazyList[Input]): Output = {
     val movements = input.map {
-      case Tuple2(dir, amt) if dir == "forward" => (amt, 0)
-      case Tuple2(dir, amt) if dir == "up" => (0, -amt)
-      case Tuple2(dir, amt) if dir == "down" => (0, amt)
+      case Forward(i) => (i, 0)
+      case Up(i) => (0, -i)
+      case Down(i) =>(0, i)
+      case _ => (0, 0)
     }
 
     val position = movements.reduce((a, b) => (a._1 + b._1, a._2 + b._2))
@@ -23,11 +33,12 @@ object Day2 extends Day {
     var position = (0, 0)
     var dir = 0
 
-    input.foreach(i => i._1 match {
-      case "forward" => position = (position._1 + i._2, position._2 + (dir * i._2))
-      case "up" => dir -= i._2
-      case "down" => dir += i._2
-    })
+    input.foreach {
+      case Forward(i) => position = (position._1 + i, position._2 + (dir * i))
+      case Up(i) => dir -= i
+      case Down(i) => dir += i
+      case _ => ()
+    }
 
     position._1 * position._2
   }
